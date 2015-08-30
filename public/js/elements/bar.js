32b0f7066c7df9420024e7b95298a04824b536eb
Jnani Weibel <jnaneshwar1234@gmail.com>
2015-08-30 01:24:22 -0700

    Fixed issue with static variables for multiple loading bars

--- public/js/elements/bar.js
+++ public/js/elements/bar.js
@@ -1,6 +1,4 @@
 (function() {
-	var bar, outline;
-
 	function Bar(width, height, padding, fillColor, frameColor) {
 		this.Container_constructor();
 
@@ -10,23 +8,23 @@
 		this.fillColor = fillColor;
 		this.frameColor = frameColor;
 
+		this.bar = new createjs.Shape()
+		this.outline = new createjs.Shape();
+
 		this.setup();
 	}
 
 	var p = createjs.extend(Bar, createjs.Container)
 
 	p.setup = function() {
-		bar = new createjs.Shape();
-		outline = new createjs.Shape();
+		this.bar.graphics.beginFill(this.fillColor).drawRect(0, 0, 1, this.height).endFill();
+		this.outline.graphics.beginStroke(this.frameColor).drawRect(-this.padding, -this.padding, this.width + (2 * this.padding), this.height + (2 * this.padding));
 
-		bar.graphics.beginFill(this.fillColor).drawRect(0, 0, 1, this.height);
-		outline.graphics.beginStroke(this.frameColor).drawRect(-this.padding, -this.padding, this.width + (2 * this.padding), this.height + (2 * this.padding));
-
-		this.addChild(bar, outline);
+		this.addChild(this.bar, this.outline);
 	}
 
 	p.update = function(progress) {
-		bar.scaleX = progress * this.width;
+		this.bar.scaleX = progress * this.width;
 	}
 
 	window.Bar = createjs.promote(Bar, "Container");
